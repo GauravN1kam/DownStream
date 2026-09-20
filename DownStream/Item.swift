e890ac5505020f47e33ca9de069904bd45224fa3
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-09-20 10:25:57 +0530

    Initial Commit

---
+++ DownStream/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DownStream
+//
+//  Created by Gaurav Nikam on 20/09/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
